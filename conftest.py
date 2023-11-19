6acfbf729c201e20154df6fd894226cd75aa89fe
Pankaj Dhapola <dhapolapankaj@users.noreply.github.com>
2023-11-19 17:52:05 +0530

    making chromium headless

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import Playwright
 @pytest.fixture(autouse=True, scope="session")
 def before_after(playwright: Playwright):
     #we will have to get things ready before tests
-    browser =  playwright.chromium.launch(headless=False, slow_mo=2000)
+    browser =  playwright.chromium.launch(headless=True, slow_mo=2000)
     context =  browser.new_context()
     page =  context.new_page()
     yield page
